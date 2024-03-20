58c148a7a30018ef3aead5f2760e6f9d3ccb4c81
wangyw15 <wangyanwei15@outlook.com>
2024-03-21 00:32:48 +0800

    Restore language selection on Android

--- patcher/patcher_android.py
+++ patcher/patcher_android.py
@@ -56,6 +56,16 @@ def patch_globals_lua(code: str) -> str:
     for line in original_code_lines:
         if re.fullmatch(r"\s*loadstring\(\S*\)\(\)\s*", line):
             patched_code_lines.append("--" + line)
+            patched_code_lines.append("\n".join(""""
+    if love.system.getOS() == 'Android' then
+        self.F_DISCORD = false
+        self.F_SAVE_TIMER = 5
+        self.F_ENGLISH_ONLY = false
+        self.F_CRASH_REPORTS = false
+        self.F_SKIP_TUTORIAL = false
+    end
+
+            """.split("\n")[1:-1]))
         else:
             patched_code_lines.append(line)
 
