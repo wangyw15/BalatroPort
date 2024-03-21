9766b69c35a3cfba92d614749ff6569c2bc68eba
wangyw15 <wangyanwei15@outlook.com>
2024-03-21 18:27:12 +0800

    Fix already initialized error

--- patcher/patcher_android.py
+++ patcher/patcher_android.py
@@ -7,14 +7,18 @@ def patch_globals_lua(code: str) -> str:
 
     for line in original_code_lines:
         if re.fullmatch(r"\s*loadstring\(\S*\)\(\)\s*", line):
+            # enable the game to run on mobile
             patched_code_lines.append("--" + line)
+            # mobile settings
             patched_code_lines.append("\n".join(""""
-    if love.system.getOS() == 'Android' then
+    if love.system.getOS() == 'Android' or love.system.getOS() == 'iOS' then
         self.F_DISCORD = false
         self.F_SAVE_TIMER = 5
         self.F_ENGLISH_ONLY = false
         self.F_CRASH_REPORTS = false
-        self.F_SKIP_TUTORIAL = false
+        self.F_SKIP_TUTORIAL = true
+        self.F_VIDEO_SETTINGS = false
+        self.F_QUIT_BUTTON = false
     end
 
             """.split("\n")[1:-1]))
