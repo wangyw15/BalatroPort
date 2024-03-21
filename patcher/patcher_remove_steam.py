525de183cfbc089de372dbfa5e4f5bb88d8acf98
wangyw15 <wangyanwei15@outlook.com>
2024-03-21 21:54:52 +0800

    Simplify remove_steam patcher

--- patcher/patcher_remove_steam.py
+++ patcher/patcher_remove_steam.py
@@ -10,40 +10,19 @@ def patch_main_lua(code: str) -> str:
     original_code_lines = code.split("\n")
     patched_code_lines: list[str] = []
 
-    function_love_load = 0
-    steam_integration = False
-    function_love_quit = 0
-
-    code_level = 0
+    function_love_load = False
 
     for line in original_code_lines:
-        # calculate code level
-        if not line.strip().startswith("--"):
-            if re.fullmatch(r"\s*(function|if).*", line):
-                code_level += 1
-            elif re.fullmatch(r"\s*end\s*", line):
-                code_level -= 1
-
-        remove_line = False
-
         if re.fullmatch(r"\s*function\s+love\.load\(\s*\)\s*", line):
-            function_love_load = code_level
-        if re.fullmatch(r"\s*function\s+love\.quit\(\s*\)\s*", line):
-            function_love_quit = code_level
+            function_love_load = True
 
-        if function_love_load:
-            if re.fullmatch(r"\s*--Steam\sintegration\s*", line):
-                steam_integration = True
-            remove_line = steam_integration
-            if re.fullmatch(r"\s*end\s*", line) and code_level == function_love_load:
-                steam_integration = False
-                function_love_load = 0
-
-        if function_love_quit:
-            if re.fullmatch(r"\s*if\s*G\.STEAM\s*then\s*G\.STEAM:shutdown\(\s*\)\s*end\s*", line):
-                remove_line = True
-                function_love_quit = 0
-
-        patched_code_lines.append(("--" if remove_line else "") + line)
+        if (function_love_load and
+                re.fullmatch(r"\s*if\s+os\s+==\s+'OS X'\s+or\s+os\s+==\s+'Windows'\s+then\s*", line)):
+            patched_code_lines.append("--" + line)
+            patched_code_lines.append(re.compile(r"os\s+==\s+'OS X'\s+or\s+os\s+==\s+'Windows'")
+                                        .sub("false", line))
+            function_love_load = False
+            continue
+        patched_code_lines.append(line)
 
     return "\n".join(patched_code_lines)
