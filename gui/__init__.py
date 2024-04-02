53412280b28622dcda8c520feed651020fdb9a1b
wangyw15 <wangyanwei15@outlook.com>
2024-04-02 14:19:36 +0800

    Add zh_CN

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtCore import QCoreApplication
+from PySide6.QtCore import QCoreApplication, QTranslator, QLocale
 from PySide6.QtWidgets import (
     QApplication,
 )
@@ -18,11 +18,21 @@ class Window(MSFluentWindow):
         super().__init__()
         self.setWindowTitle(QCoreApplication.translate("Window", "Balatro Patcher"))
         self.config = Config()
-        self.addSubInterface(PatcherWidget(self), FluentIcon.APPLICATION, "Patcher")
+        self.addSubInterface(
+            PatcherWidget(self),
+            FluentIcon.APPLICATION,
+            QCoreApplication.translate("Window", "Patcher"),
+        )
 
 
 def run():
     app = QApplication([])
+
+    # language translation
+    translator = QTranslator()
+    if translator.load(f"{QLocale.system().name()}.qm", directory="i18n"):
+        app.installTranslator(translator)
+
     window = Window()
     window.show()
     sys.exit(app.exec())
---
+++ i18n/zh_CN.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Patcher</name>
+    <message>
+        <location filename="../gui/patcher.py" line="39"/>
+        <location filename="../gui/patcher.py" line="43"/>
+        <source>Browse</source>
+        <translation>浏览</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="48"/>
+        <source>Output type</source>
+        <translation>保存类型</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="51"/>
+        <source>Select the type of output file you want to generate.</source>
+        <translation>选择文件输出的类型。</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="55"/>
+        <source>Patch</source>
+        <translation>修补</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="66"/>
+        <source>Select patchers</source>
+        <translation>选择补丁</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="75"/>
+        <source>Select Balatro.exe</source>
+        <translation>选择 Balatro.exe</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="90"/>
+        <source>Path to save</source>
+        <translation>保存路径</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="136"/>
+        <location filename="../gui/patcher.py" line="148"/>
+        <source>No file selected</source>
+        <translation>未选择文件</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="139"/>
+        <source>No Balatro.exe selected.</source>
+        <translation>未选择 Balatro.exe 文件。</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="149"/>
+        <source>No save file selected.</source>
+        <translation>没有选择保存路径。</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="164"/>
+        <source>File not found</source>
+        <translation>文件不存在</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="167"/>
+        <source>Selected Balatro.exe not found.</source>
+        <translation>选择的 Balatro.exe 不存在。</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="180"/>
+        <source>Success</source>
+        <translation>成功</translation>
+    </message>
+    <message>
+        <location filename="../gui/patcher.py" line="181"/>
+        <source>Patching complete.</source>
+        <translation>修补完成。</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../gui/__init__.py" line="19"/>
+        <source>Balatro Patcher</source>
+        <translation>小丑牌助手</translation>
+    </message>
+    <message>
+        <location filename="../gui/__init__.py" line="24"/>
+        <source>Patcher</source>
+        <translation>修补工具</translation>
+    </message>
+</context>
+</TS>
